2e332701def89a4f47e61934c4e050da76f41f9b
Krijnse Locker <richard.krijnse-locker@capgemini.com>
2017-10-05 10:30:31 +0200

    5-10

---
+++ src/main/resources/public/js/rooms.js
@@ -0,0 +1,57 @@
+var table = $("#table").DataTable();
+
+$("#linkAddRoom").click(function (e) {
+    e.preventDefault();
+    $("#roomModal").modal("toggle");
+});
+
+getAll();
+
+$("#btnClose").click(function() {
+ $("#roomModal").modal("hide");
+});
+
+function getAll() {
+    $.get("/api/rooms/", function (result) {
+        console.log(result);
+        table.clear();
+        for (var i = 0; i < result.length; i++) {
+            table.row.add(["<a href=\"javascript:del(" + result[i].roomID + ")\"><i class='fa fa-trash-o' aria-hidden='true'></i></a>",
+                            "<a href=\"javascript:edit("+result[i].roomID+")\">"+result[i].roomID+"</a>",
+                            result[i].roomSize,
+                            result[i].roomSize,
+                            result[i].dateReady,
+                            result[i].roomNumber,
+        }
+        table.draw();
+    });
+}
+
+$("#btnAddRoom").click(function() {
+    console.log("test");
+    var obj = {};
+    obj.roomID = $("#id").val();
+    obj.roomSize = $("#roomSize").val();
+    obj.roomType =  $("#roomType").val();
+    obj.dateReady = $("#dateReady").val();
+    obj.roomAvailable = $("#roomAvailable").val();
+    obj.roomNumber = $("#roomNumber").val();
+
+    console.log(obj);
+
+    $.ajax({
+        url: "/api/rooms/",
+        type: "POST",
+        data: JSON.stringify(obj),
+        contentType: "application/json; charset=utf-8",
+        success: function(result) {
+            console.log(result);
+            return result;
+        },
+        error: function(err) {
+            console.log(err.responseText);
+        }
+    });
+    $("#roomModal").modal("hide");
+});
+

1cb2004d6349517c96e6b8e2b5b203deab64bd8f
van Oostrom <aaricia.van-oostrom@capgemini.com>
2017-10-10 11:09:42 +0200

    update

--- src/main/java/com/capgemini/hotel/Room.java
+++ src/main/java/com/capgemini/hotel/Room.java
@@ -20,7 +20,7 @@ public class Room {
 
     protected Room() {}
 
-    public Room( ERoomSize roomSize, ERoomType roomType, boolean roomAvailable, LocalDate dateReady, String roomNumber) {
+    public Room(ERoomSize roomSize, ERoomType roomType, boolean roomAvailable, LocalDate dateReady, String roomNumber) {
         this.roomSize = roomSize;
         this.roomType = roomType;
         this.dateReady = dateReady;
--- src/main/resources/public/js/rooms.js
+++ src/main/resources/public/js/rooms.js
@@ -13,11 +13,13 @@ function getAll() {
     $.get("/api/rooms/", function (result) {
         table.clear();
         for (var i = 0; i < result.length; i++) {
+            var available = (result[i].roomAvailable) ? "Ja" : "Nee";
             table.row.add(["<a href=\"javascript:del(" + result[i].roomID + ")\"><font color='#ff3385'><i class='fa fa-trash-o' aria-hidden='true'></i></font></a>",
                             "<a href=\"javascript:edit("+result[i].roomID+")\"><font color='#ff3385'><i class='fa fa-pencil' aria-hidden='true'></i></font></a>",
                             result[i].roomSize,
                             result[i].roomType,
                             result[i].roomNumber,
+                            available,
                             (result[i].dateReady[2]+"/"+result[i].dateReady[1]+"/"+result[i].dateReady[0])]);
         }
         table.draw();
@@ -32,9 +34,10 @@ $("#btnClose").click(function() {
 $("#btnAddRoom").click( function (e) {
     e.preventDefault();
     var obj = getObject();
+    console.log(obj);
     $.ajax({
         url: "/api/rooms/",
-        method:"PUT",
+        method:"POST",
         data: JSON.stringify(obj),
         contentType: "application/json; charset=utf-8"
         }).done(function () {
@@ -77,12 +80,17 @@ function del(id) {
 }
 
 function edit(id) {
+    $("#btnAddRoom").hide();
+    $("#btnUpdateRoom").show();
     $.get({url:"/api/rooms/"+id+"/", type:"GET"}).done( function(result) {
+        var available = result.roomAvailable;
         $("#id").val(result.roomID);
         $("#roomSize").val(result.roomSize);
         $("#roomType").val(result.roomType);
         $("#dateReady").val(result.dateReady);
         $("#roomNumber").val(result.roomNumber);
+        $('#roomAvailable option:contains(' +  available + ')').prop({selected: true});
+        $("#roomModal").modal("toggle");
     })
 }
 
@@ -91,8 +99,9 @@ function getObject() {
     obj.roomSize = $("#roomSize").val();
     obj.roomType =  $("#roomType").val();
     obj.dateReady = $("#dateReady").val();
-    obj.roomAvailable = $("#roomAvailable").val();
+    obj.roomAvailable = ($("#roomAvailable").val()=="Ja") ? true : false;
     obj.roomNumber = $("#roomNumber").val();
+    obj.roomID = $("#id").val();
     return obj;
 }
 
